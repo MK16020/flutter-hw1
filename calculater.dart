579098ed6ba58ac6a75db4654e42a8a316d07363
MK16020 <61877101+MK16020@users.noreply.github.com>
2023-01-15 23:53:21 +0300

    update

--- calculater.dart
+++ calculater.dart
@@ -1,59 +1,64 @@
 import 'dart:io';
 
-void main() 
-{
-  var firstDigit;
-  var secondDigit;
+void main() {
+  var firstDigit = 0;
+  var secondDigit = 0;
   var result;
   var operation;
 
-  do{
+  do {
     print("Enter the first integer");
-    int firstDigit = int.parse(stdin.readLineSync());
+    firstDigit = int.parse(stdin.readLineSync()!);
     print("Enter the second integer");
-    int secondDigit = int.parse(stdin.readLineSync());
-    print("Choose the operation: \n ● * \n ● + \n ● - \n ● / \n ● % \n ● x to exit");
+    secondDigit = int.parse(stdin.readLineSync()!);
+    print(
+        "Choose the operation: \n ● * \n ● + \n ● - \n ● / \n ● % \n ● x to exit");
     operation = (stdin.readLineSync());
-    switch(operation){
-      case '*':{
-        result = firstDigit * secondDigit;
-        print("\n the result is $result \n\n");
-      }
-      break;
+    switch (operation) {
+      case '*':
+        {
+          result = firstDigit * secondDigit;
+          print("\n the result is $result \n\n");
+        }
+        break;
 
-      case '+':{
-        result = firstDigit + secondDigit;
-        print("\n the result is $result \n\n");
-      }
-      break;
+      case '+':
+        {
+          result = firstDigit + secondDigit;
+          print("\n the result is $result \n\n");
+        }
+        break;
 
-      case '-':{
-        result = firstDigit - secondDigit;
-        print("\n the result is $result \n\n");
-      }
-      break;
+      case '-':
+        {
+          result = firstDigit - secondDigit;
+          print("\n the result is $result \n\n");
+        }
+        break;
 
-      case '/':{
-        result = firstDigit / secondDigit;
-        print("\n the result is $result \n\n");
-      }
-      break;
+      case '/':
+        {
+          result = firstDigit / secondDigit;
+          print("\n the result is $result \n\n");
+        }
+        break;
 
-      case '%':{
-        result = firstDigit % secondDigit;
-        print("\n the result is $result \n\n");
-      }
-      break;
+      case '%':
+        {
+          result = firstDigit % secondDigit;
+          print("\n the result is $result \n\n");
+        }
+        break;
 
-      case 'x':{
-        print("\n Bye \n\n");
-      }
-      break;
-      default:{
-        print(" Wrong Entery");
-      }
+      case 'x':
+        {
+          print("\n Bye \n\n");
+        }
+        break;
+      default:
+        {
+          print(" Wrong Entery");
+        }
     }
-
-    
-  }while(operation != 'x');
+  } while (operation != 'x');
 }
